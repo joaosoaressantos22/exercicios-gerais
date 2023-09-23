25fb6a77cfa6c356143106c5f5556c0b7413acf7
Joao <jotavesantos1@gmail.com>
2023-09-23 15:22:23 -0300

    Testando a key ssh

---
+++ 05_ponteiros/pont_06/Includes/data.c
@@ -0,0 +1,61 @@
+#include "data.h" 
+
+void InicializaDataParam( int dia, int mes, int ano, tData *data);
+
+/**
+ * @brief Lê uma data do usuário.
+ * 
+ * Esta função lê do usuário o dia, mês e ano de uma data e armazena esses valores na estrutura tData correspondente.
+ * 
+ * @param data Ponteiro para a estrutura tData que será preenchida com os valores lidos.
+ */
+void LeData( tData *data );
+
+/**
+ * @brief Imprime uma data na tela.
+ * 
+ * Esta função recebe como parâmetro uma estrutura tData e imprime na tela o dia, mês e ano correspondentes.
+ * 
+ * @param data Ponteiro para a estrutura tData que será impressa.
+ */
+void ImprimeData( tData *data );
+
+/**
+ * @brief Verifica se um ano é bissexto.
+ * 
+ * Esta função recebe como parâmetro uma estrutura tData e verifica se o ano correspondente é bissexto.
+ * 
+ * @param data Ponteiro para a estrutura tData que será verificada.
+ * @return 1 se o ano é bissexto, 0 caso contrário.
+ */
+int EhBissexto( tData *data );
+
+/**
+ * @brief Informa a quantidade de dias no mês de uma data.
+ * 
+ * Esta função recebe como parâmetro uma estrutura tData e informa a quantidade de dias no mês correspondente.
+ * 
+ * @param data Ponteiro para a estrutura tData que será verificada.
+ * @return Quantidade de dias no mês correspondente.
+ */
+int InformaQtdDiasNoMes( tData *data );
+
+/**
+ * @brief Avança uma data para o dia seguinte.
+ * 
+ * Esta função recebe como parâmetro uma estrutura tData e avança a data correspondente para o dia seguinte.
+ * 
+ * @param data Ponteiro para a estrutura tData que será avançada.
+ */
+void AvancaParaDiaSeguinte( tData *data );
+
+/**
+ * @brief Verifica se duas datas são iguais.
+ * 
+ * Esta função recebe como parâmetro duas estruturas tData e verifica se elas representam a mesma data.
+ * 
+ * @param data1 Ponteiro para a primeira estrutura tData que será comparada.
+ * @param data2 Ponteiro para a segunda estrutura tData que será comparada.
+ * @return 1 se as datas são iguais, 0 caso contrário.
+ */
+int EhIgual( tData *data1, tData *data2 );

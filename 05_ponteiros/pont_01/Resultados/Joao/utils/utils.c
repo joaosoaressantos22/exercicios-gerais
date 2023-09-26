0ec7d8b32dde273f2b37186ab9a6a7e788e391b0
Joao <jotavesantos1@gmail.com>
2023-09-25 21:54:32 -0300

    Testando o primeiro ponteiro 1

---
+++ 05_ponteiros/pont_01/Resultados/Joao/utils/utils.c
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+int TrocaInterios(int * n1, int * n2){
+    int numero;
+    numero = *n1;
+    *n1 = *n2;
+    *n2 = numero;
+    if (*n2 == numero){
+        return 1; 
+    }
+    else {
+        return 0;
+    }
+}

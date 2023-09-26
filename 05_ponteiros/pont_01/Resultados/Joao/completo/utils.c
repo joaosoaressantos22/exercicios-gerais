b25dce738f1c58820715b57fa04f5a250e5215c3
Joao <jotavesantos1@gmail.com>
2023-09-25 22:11:49 -0300

    Ponteiro 01 corrigido

--- 05_ponteiros/pont_01/Resultados/Joao/completo/utils.c
+++ 05_ponteiros/pont_01/Resultados/Joao/completo/utils.c
@@ -1,11 +1,11 @@
 #include "utils.h"
 
 int TrocaInterios(int * n1, int * n2){
-    int numero;
-    numero = *n1;
-    *n1 = *n2;
-    *n2 = numero;
-    if (*n2 == numero){
+    if (*n1 > *n2){
+        int numero;
+        numero = *n1;
+        *n1 = *n2;
+        *n2 = numero;
         return 1; 
     }
     else {

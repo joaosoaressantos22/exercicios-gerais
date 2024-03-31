a750ada0f9fcf3e1cfcd37e25503cfaf5acbc46e
João Vitor Soares dos Santos <90335051+joaosoaressantos22@users.noreply.github.com>
2024-03-31 02:05:39 -0300

    Update main.c

--- 04_TAD_simples/TAD_03/Respostas/Joao/main.c
+++ 04_TAD_simples/TAD_03/Respostas/Joao/main.c
@@ -1,11 +1,33 @@
 #include "locadora.h"
 #include "filme.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    int idade;
-    tLocadora locadora = criarLocadora();
-    locadora = lerCadastroLocadora(locadora);
-    locadora = lerAluguelLocadora(locadora);
+    char string[100];
+    tLocadora locadora = criarLocadora();   
+    while (1){
+        scanf("%s", string);
+        switch (string[0])
+        {
+        case 'C':
+            locadora = lerCadastroLocadora(locadora);
+            break;
+        case 'A':
+            locadora = lerAluguelLocadora(locadora);
+            break;
+        case 'D':
+            locadora = lerDevolucaoLocadora(locadora);
+            break;
+        case 'E':
+            consultarEstoqueLocadora(locadora);
+            break;
+        case 'S':
+            return 0;
+        default:
+            break;
+        }
+    }
     return 0;
+
 }

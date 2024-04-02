19c3684457e93e4c0bc229937f3d03ba25d72ebe
Joao <jotavesantos1@gmail.com>
2024-04-02 00:18:09 -0300

    Resolvendo as bibliotecas

--- 03_bibliotecas/bibli_01/Includes/main.c
+++
@@ -1,9 +0,0 @@
-#include "esfera_utils.h"
-#include <stdio.h>
-float R;  
-int main(){
-    scanf("%f", &R);
-    printf("A area é: %f\n",calcula_area(R));  
-    printf("O volume da esfera é: %f\n", calcula_volume(R)); 
-    return 0; 
-}
--- 03_bibliotecas/bibli_01/Resultados/Joao/completo/main.c
+++
@@ -1,9 +0,0 @@
-#include "esfera_utils.h"
-#include <stdio.h>
-float R;  
-int main(){
-    scanf("%f", &R);
-    printf("Area: %.2f\nVolume: %.2f\n", 
-            calcula_area(R), calcula_volume(R));
-    return 0; 
-}

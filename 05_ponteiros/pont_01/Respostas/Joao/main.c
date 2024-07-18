ebcf3766c341284bfab152545490135165b51e68
João Vitor Soares dos Santos <90335051+joaosoaressantos22@users.noreply.github.com>
2024-07-18 20:04:09 -0300

    Update main.c

--- 05_ponteiros/pont_01/Respostas/Joao/main.c
+++ 05_ponteiros/pont_01/Respostas/Joao/main.c
@@ -6,16 +6,16 @@
 //Defina e implemente TrocaInterios
 int main(){
 
-    int n1, n2;
+    int n1, n2, *p1, *p2;
 
-    while ( scanf("%d %d", &n1, &n2) == 2 ){
-        
-        if ( TrocaInterios( &n1, &n2 ) ){
-            printf("TROCADO: %d %d\n", n1, n2);
+    while (scanf("%d %d", &n1, &n2) == 2){
+        p1 = &n1, p2 = &n2;
+        if (TrocaInterios(p1, p2) == 1){
+            printf("TROCADO: %d %d\n", *p1, *p2);
         } 
-        else {
+        else{
             printf("ORIGINAL: %d %d\n", n1, n2);
         }
-        }
-            return 0; 
+    }
+    return 0; 
 }
